3ef26cae8e0316eb2ee238bde88ab0e3c2bc5a1a
unknown <Volodimir>
2024-02-11 14:15:11 +0200

    sanday11-02-2024

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+
+const ul = document.querySelector("#categories");
+const animals = ul.children[0];
+const Products = ul.children[1]
+const Technologies = ul.children[2]
+console.log(animals
+)
+console.log(Products)
+console.log(Technologies)
