cf773149819679297c6e47b87f5b316f3514a500
Volodimyr213 <Terminal213@gmail.com>
2024-02-12 13:01:29 +0200

    OK

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,17 @@
 
 const ul = document.querySelector ("#categories");
-const animals = ul.children[0];
-const Products = ul.children[1]
-const Technologies = ul.children[2]
-console.log(animals
-)
-console.log(Products)
-console.log(Technologies)
+const animals = ul.children.length;
+
+console.log(`Number of categories:${animals}`)
+
+
+const categorys = document.querySelectorAll(".item") 
+
+
+categorys.forEach(elem=>{
+    
+   const categori = elem.firstElementChild.textContent
+   const elements = elem.lastElementChild.children.length
+   console.log(`Category:${categori}`)    
+   console.log(`Elements:${elements}`)
+})
