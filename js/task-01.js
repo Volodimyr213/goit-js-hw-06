894baddfd2a903aa591ecd8f28d279d05bf70700
Volodimyr213 <Terminal213@gmail.com>
2024-02-12 11:41:20 +0200

    in-home

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,5 @@
 
-const ul = document.querySelector("#categories");
+const ul = document.querySelector ("#categories");
 const animals = ul.children[0];
 const Products = ul.children[1]
 const Technologies = ul.children[2]
